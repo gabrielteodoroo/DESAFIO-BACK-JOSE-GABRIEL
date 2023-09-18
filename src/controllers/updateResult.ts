dd3aeb253a10089c7bc3b4901e495a7a5f05169f
gabrielteodoroo <biel.teodorob@gmail.com>
2023-09-18 17:53:37 -0300

    Desafio finalizado

--- src/controllers/updateResult.ts
+++ src/controllers/updateResult.ts
@@ -3,47 +3,49 @@ import { db } from "../services/dbConnection";
 import { Disciplina } from "@prisma/client";
 
 export const updateResult = async (req: Request, res: Response) => {
-    try {
-      const { id, disciplina, nota } = req.body;
-  
-      if (!id) {
-        return res.status(400).json({ message: "Informe o id" });
-      }
-  
-      const findResult = await db.resultado.findFirst({ where: { id } });
-  
-      if (!findResult) {
-        return res.status(400).json({ message: "resultado não encontrado" });
-      }
-  
-      if (isNaN(parseFloat(nota)) || nota < 0 || nota > 10) {
-        return res.status(400).json({ message: "Informe uma nota válida" });
-      }
-  
-      if (!Object.values(Disciplina).includes(disciplina)) {
-        return res.status(400).json({ message: "Disciplina inválida" });
-      }
-  
-      const findDisciplina = await db.resultado.findFirst({
-        where: { disciplina },
-      });
-  
-      if (findDisciplina?.disciplina !== findResult.disciplina) {
-        return res.status(400).json({
-          message: "Já existe um resultado para a disciplina informada",
-        });
-      }
-  
-      const resultUpdated = await db.resultado.update({
-        where: { id },
-        data: {
-          disciplina,
-          nota,
-        },
-      });
-  
-      return res.status(200).json(resultUpdated);
-    } catch (error) {
-      return res.status(500).json({ message: "Erro interno do servidor." });
+  try {
+    const { bimestre, disciplina, nota } = req.body;
+
+    if (!bimestre) {
+      return res.status(400).json({ message: "Informe o id" });
     }
-  };
+
+    const findResult = await db.resultado.findFirst({
+      where: { bimestre, disciplina },
+    });
+
+    if (!findResult) {
+      return res.status(400).json({ message: "resultado não encontrado" });
+    }
+
+    if (isNaN(parseFloat(nota)) || nota < 0 || nota > 10) {
+      return res.status(400).json({ message: "Informe uma nota válida" });
+    }
+
+    if (!Object.values(Disciplina).includes(disciplina)) {
+      return res.status(400).json({ message: "Disciplina inválida" });
+    }
+
+    const findDisciplina = await db.resultado.findFirst({
+      where: { disciplina, bimestre },
+    });
+
+    if (findDisciplina?.disciplina !== findResult.disciplina) {
+      return res.status(400).json({
+        message: "Já existe um resultado para a disciplina informada",
+      });
+    }
+
+    const resultUpdated = await db.resultado.update({
+      where: { id: findDisciplina.id },
+      data: {
+        disciplina,
+        nota,
+      },
+    });
+
+    return res.status(200).json(resultUpdated);
+  } catch (error) {
+    return res.status(500).json({ message: "Erro interno do servidor." });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import "dotenv/config";
 import router from "./router";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
